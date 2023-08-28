9e59048e258eea7358d4a1d11c22f9266788210e
symberprzemyslaw <symber.przemyslaw@gmail.com>
2023-08-28 22:39:06 +0200

    Added logic and based styling

---
+++ index.js
@@ -0,0 +1,30 @@
+const correctAnserws = ['B','B','B','B']
+const form = document.querySelector('.quiz-form')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    let score = 0
+    const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value]
+
+    //check answers
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnserws[index]){
+            score += 25
+        } 
+    })
+    scrollTo(0,0)
+    document.querySelector('.result').classList.remove('d-none')
+  
+
+    let output = 0
+    const timer = setInterval(() => {
+        document.querySelector('span').textContent = `${output} %`
+        if(output === score){
+            clearInterval(timer)
+        } else {
+            output++
+        }
+    },10 )
+})
+
+
